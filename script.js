54bf42100c8f5686d294890426020024d0b7f084
y0sh1da <161025621+y0sh1da-available@users.noreply.github.com>
2026-03-06 12:42:49 +0700

    Change greeting text for International Women's Day

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function drawText() {
 
     if(frameNumber >= 3000 && frameNumber < 99999){
         context.fillStyle = `rgba(252, 208, 218, ${thirdOpacity})`;
-        context.fillText("Happy Valentine's Day <3", canvas.width/2, (canvas.height/2 + 120));
+        context.fillText("Happy International Women's Day <3", canvas.width/2, (canvas.height/2 + 120));
         thirdOpacity = thirdOpacity + 0.01;
 
         button.style.display = "block";
